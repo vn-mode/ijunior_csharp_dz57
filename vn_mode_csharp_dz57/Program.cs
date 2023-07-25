ebd1058a810db0e1bafec40aedcc824e805ccf9e
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-25 20:31:22 +0300

    ijunior_csharp_dz57

--- vn_mode_csharp_dz57/Program.cs
+++ vn_mode_csharp_dz57/Program.cs
@@ -2,6 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        var squad1 = new Squad();
+        squad1.AddSoldier(new Soldier("Белов"));
+        squad1.AddSoldier(new Soldier("Алексеев"));
+        squad1.AddSoldier(new Soldier("Быков"));
+
+        var squad2 = new Squad();
+        squad2.AddSoldier(new Soldier("Сидоров"));
+        squad2.AddSoldier(new Soldier("Кузнецов"));
+
+        Console.WriteLine("Отряд 1 до перевода:");
+        squad1.PrintSquad();
+
+        Console.WriteLine("\nОтряд 2 до перевода:");
+        squad2.PrintSquad();
+
+        squad1.TransferSoldiers(squad2, soldier => soldier.Name.StartsWith("Б"));
+
+        Console.WriteLine("\nОтряд 1 после перевода:");
+        squad1.PrintSquad();
+
+        Console.WriteLine("\nОтряд 2 после перевода:");
+        squad2.PrintSquad();
+    }
+}
+
 public class Soldier
 {
     public string Name { get; private set; }
@@ -14,66 +43,31 @@ public class Soldier
 
 public class Squad
 {
-    private List<Soldier> soldiers;
+    private List<Soldier> _soldiers;
 
     public Squad()
     {
-        soldiers = new List<Soldier>();
+        _soldiers = new List<Soldier>();
     }
 
-    public List<Soldier> GetSoldiers()
+    public void AddSoldier(Soldier soldier)
     {
-        return soldiers;
+        _soldiers.Add(soldier);
+    }
+
+    public void TransferSoldiers(Squad toSquad, Func<Soldier, bool> predicate)
+    {
+        var soldiersToTransfer = _soldiers.Where(predicate).ToList();
+
+        _soldiers = _soldiers.Except(soldiersToTransfer).ToList();
+        toSquad._soldiers.AddRange(soldiersToTransfer);
     }
 
     public void PrintSquad()
     {
-        foreach (var soldier in soldiers)
+        foreach (var soldier in _soldiers)
         {
             Console.WriteLine(soldier.Name);
         }
-    }
-}
-
-class Program
-{
-    private const string TransferNameStart = "Б";
-
-    static void Main(string[] args)
-    {
-        var squad1 = new Squad();
-        squad1.GetSoldiers().AddRange(new List<Soldier> {
-            new Soldier("Белов"),
-            new Soldier("Алексеев"),
-            new Soldier("Быков")
-        });
-
-        var squad2 = new Squad();
-        squad2.GetSoldiers().AddRange(new List<Soldier> {
-            new Soldier("Сидоров"),
-            new Soldier("Кузнецов")
-        });
-
-        Console.WriteLine("Отряд 1 до перевода:");
-        squad1.PrintSquad();
-
-        Console.WriteLine("\nОтряд 2 до перевода:");
-        squad2.PrintSquad();
-
-        TransferSoldiers(squad1, squad2);
-
-        Console.WriteLine("\nОтряд 1 после перевода:");
-        squad1.PrintSquad();
-
-        Console.WriteLine("\nОтряд 2 после перевода:");
-        squad2.PrintSquad();
-    }
-
-    private static void TransferSoldiers(Squad fromSquad, Squad toSquad)
-    {
-        var soldiersToTransfer = fromSquad.GetSoldiers().Where(soldier => soldier.Name.StartsWith(TransferNameStart)).ToList();
-
-        fromSquad.GetSoldiers().RemoveAll(soldier => soldiersToTransfer.Contains(soldier));
-        toSquad.GetSoldiers().AddRange(soldiersToTransfer);
     }
 }
