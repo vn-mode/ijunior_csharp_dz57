d78a74575be4ff4764fd7ab0ddb358f0e3f9a32c
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-08-17 23:39:26 +0300

    ijunior_csharp_dz57

--- vn_mode_csharp_dz57/Program.cs
+++ vn_mode_csharp_dz57/Program.cs
@@ -2,13 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Squad squad1 = new Squad();
+        squad1.AddSoldier(new Soldier("Белов"));
+        squad1.AddSoldier(new Soldier("Алексеев"));
+        squad1.AddSoldier(new Soldier("Быков"));
+
+        Squad squad2 = new Squad();
+        squad2.AddSoldier(new Soldier("Сидоров"));
+        squad2.AddSoldier(new Soldier("Кузнецов"));
+
+        Console.WriteLine("Отряд 1 до перевода:");
+        squad1.PrintSquad();
+
+        Console.WriteLine("\nОтряд 2 до перевода:");
+        squad2.PrintSquad();
+
+        IEnumerable<Soldier> soldiersToTransfer = squad1.ExtractSoldiers(soldier => soldier.Name.StartsWith("Б"));
+        squad2.IncludeSoldiers(soldiersToTransfer);
+
+        Console.WriteLine("\nОтряд 1 после перевода:");
+        squad1.PrintSquad();
+
+        Console.WriteLine("\nОтряд 2 после перевода:");
+        squad2.PrintSquad();
+    }
+}
+
 public class Soldier
 {
-    public string Name { get; private set; }
+    private string _name;
 
     public Soldier(string name)
     {
-        Name = name;
+        _name = name;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        private set { _name = value; }
     }
 }
 
@@ -28,7 +64,7 @@ public class Squad
 
     public IEnumerable<Soldier> ExtractSoldiers(Func<Soldier, bool> predicate)
     {
-        var soldiersToExtract = _soldiers.Where(predicate).ToList();
+        List<Soldier> soldiersToExtract = _soldiers.Where(predicate).ToList();
         _soldiers.RemoveAll(soldier => soldiersToExtract.Contains(soldier));
         return soldiersToExtract;
     }
@@ -44,35 +80,5 @@ public class Squad
         {
             Console.WriteLine(soldier.Name);
         }
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        var squad1 = new Squad();
-        squad1.AddSoldier(new Soldier("Белов"));
-        squad1.AddSoldier(new Soldier("Алексеев"));
-        squad1.AddSoldier(new Soldier("Быков"));
-
-        var squad2 = new Squad();
-        squad2.AddSoldier(new Soldier("Сидоров"));
-        squad2.AddSoldier(new Soldier("Кузнецов"));
-
-        Console.WriteLine("Отряд 1 до перевода:");
-        squad1.PrintSquad();
-
-        Console.WriteLine("\nОтряд 2 до перевода:");
-        squad2.PrintSquad();
-
-        var soldiersToTransfer = squad1.ExtractSoldiers(soldier => soldier.Name.StartsWith("Б"));
-        squad2.IncludeSoldiers(soldiersToTransfer);
-
-        Console.WriteLine("\nОтряд 1 после перевода:");
-        squad1.PrintSquad();
-
-        Console.WriteLine("\nОтряд 2 после перевода:");
-        squad2.PrintSquad();
     }
 }
